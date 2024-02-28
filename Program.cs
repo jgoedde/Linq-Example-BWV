824cceb975fb044b0df663bb77127f164b0f166f
jgoedde <129423545+jgoedde@users.noreply.github.com>
2024-02-28 20:29:33 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
 ﻿using System.Text.Json;
 
-var daten = Datenquelle.GibMirDummyDaten();
+var daten = Datenquelle.GibMirDummyDaten();A1A5-9793
 
+/*
+daten.catalogItems
+daten.catalogBrands
+daten.catalogTypes
+daten.basketItems,
+daten.orders,
+daten.buyers,
+daten.paymentMethods,
+daten.baskets
+*/
+  
 Console.WriteLine(JsonSerializer.Serialize(daten));
